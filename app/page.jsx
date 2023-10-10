8d7fe801750fa266079f1d22d40143a943f444be
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-10 16:33:57 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import styles from "./style/pages/landingPage.module.css"
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={styles.red}>Landing page</h1>
+    </>
+  );
+}
