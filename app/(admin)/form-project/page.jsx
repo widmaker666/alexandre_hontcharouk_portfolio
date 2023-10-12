68b2f23ca8fbbcfcef01d5eb296e3a17c004e87d
widmaker666 <widmaker666@msn.com>
2023-10-12 17:55:06 +0200

    le style avance

---
+++ app/(admin)/form-project/page.jsx
@@ -0,0 +1,9 @@
+const FormProjects = () => {
+    return (
+        <div>
+            ici les formulaire pour rentrer les projets
+        </div>
+    );
+}
+
+export default FormProjects;
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
-import styles from "./style/pages/landingPage.module.css"
+import styles from "./style/pages/landingPage.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1 className={styles.red}>Landing page</h1>
+      <main className={styles.main}>
+        <p>
+          Alexandre Hontcharouk is a passionate web designer, meticulous in his
+          work. He combines his passion for programming with his love of
+          photography. Always attentive to his customers' needs, he strives to
+          create simple, effective and aesthetically pleasing designs, while
+          drawing on his artistic sensibility derived from photography.
+        </p>
+      </main>
+      <section className={styles.banner}>
+        <h1>portfolio</h1>
+      </section>
+      <section className={styles.projectComponent}>
+        ici le components des projets
+      </section>
     </>
   );
 }
