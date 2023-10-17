81a2dc791cca8fb2dcaa7b008341c2b9c99a74cc
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-17 18:42:37 +0200

    prod bdd gif loader

--- app/components/AllProjects.jsx
+++ app/components/AllProjects.jsx
@@ -2,37 +2,38 @@
 
 import { useEffect, useState } from "react";
 import DivProject from "./DivProject";
+import Image from "next/image";
+import loader from "../assets/images/XVo6.gif";
 
 const AllProjects = () => {
-    const [allProjectsArray, setAllProjectsArray] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    
-    const getAllProjects = async () => {
+  const [allProjectsArray, setAllProjectsArray] = useState([]);
 
-      try {
-        setIsLoading(true);
-        const response = await fetch("/api/projects");
-        const data = await response.json(); // Utiliser un nom de variable différent
-        console.log(data);
-        setAllProjectsArray(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const getAllProjects = async () => {
+    try {
+      const response = await fetch("/api/projects");
+      const data = await response.json(); // Utiliser un nom de variable différent
+      console.log(data);
+      setAllProjectsArray(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     getAllProjects();
   }, []); // Vous n'avez pas besoin de déclencher getAllProjects lorsque allProjectsArray change
 
-  if (isLoading) {
-    return <div>isloading</div>;
+  if (!allProjectsArray || allProjectsArray.length === 0) {
+    return (
+      <Image
+        style={{ margin: "0 auto" }}
+        src={loader}
+        width={100}
+        height={100}
+      />
+    );
   }
 
-  if (!allProjectsArray || allProjectsArray.length === 0) {
-    return <p>There are no projects.</p>;
-  }
-  
   return allProjectsArray.map((project) => (
     <DivProject key={project._id} project={project} />
   ));
