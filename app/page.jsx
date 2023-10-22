0f1b35847741ff80e1f7eab61452988265d4600a
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-22 18:29:35 +0200

    responsive fini totalement

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Me from "./assets/images/me.webp";
 import styles from "./style/pages/landingPage.module.css";
