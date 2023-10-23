df8744b9dd5d188fbccd7f0267b3028eec60581e
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-23 15:51:23 +0200

    SEO

--- app/layout.js
+++ app/layout.js
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content={metadata.ogDescription} />
         <meta property="og:type" content={metadata.ogType} />
         <meta property="og:url" content={metadata.ogUrl} />
-        <meta property="og:image" content={imageOg} />
+        <meta property="og:image" content="https://i.postimg.cc/nr93TB6X/Alexandre-Hontcharouk.png"/>
         <meta property="og:image:alt" content={imageOg} />
         <meta property="og:locale" content={metadata.ogLocale} />
       </Head>
