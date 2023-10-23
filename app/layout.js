172855ed258060ba8c7d8d92bcd496f9ec01ee14
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-23 14:39:34 +0200

    SEO

--- app/layout.js
+++ app/layout.js
@@ -22,16 +22,35 @@ const titi = Titillium_Web({
 
 //* MetaData *//
 export const metadata = {
-  title: "Alexandre Hontcharouk",
-  description: "Portfolio",
-  favicon: "/favicon.ico",
+  title: "Alexandre Hontcharouk - Développeur Web Freelance à Lyon",
+  description:
+    "Découvrez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Spécialisé en JavaScript, HTML, CSS, Sass, React.js et Next.js.",
+  keywords:
+    "développeur web, front-end, freelance, JavaScript, HTML, CSS, Sass, React.js, Next.js, Lyon, Villeurbanne",
+  author: "Alexandre Hontcharouk",
+  ogTitle: "Portfolio d'Alexandre Hontcharouk - Développeur Web Freelance",
+  ogDescription:
+    "Explorez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Découvrez ses compétences en JavaScript, HTML, CSS, Sass, React.js et Next.js.",
+  ogType: "website",
+  ogUrl: "https://alexandre-hontcharouk-portfolio.vercel.app",
+  ogImage: "https://i.postimg.cc/nr93TB6X/Alexandre-Hontcharouk.png",
+  ogLocale: "fr_FR",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${vcr.variable} ${titi.variable}`}>
+    <html lang="fr" className={`${vcr.variable} ${titi.variable}`}>
       <Head>
-        <link rel="icon" href={metadata.favicon} sizes="any" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta property="og:title" content={metadata.ogTitle} />
+        <meta property="og:description" content={metadata.ogDescription} />
+        <meta property="og:type" content={metadata.ogType} />
+        <meta property="og:url" content={metadata.ogUrl} />
+        <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:locale" content={metadata.ogLocale} />
       </Head>
       <body>
         <AdminContextProvider>
