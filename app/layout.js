a5bae1fe23f8b94303403e6ef843a0b4f27000e4
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-24 16:21:55 +0200

    maj SEO image

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AdminContextProvider from "./context/AdminContext";
-import imageOg from './assets/images/AlexandreHontcharouk.webp'
+import imageOg from "./assets/images/AlexandreHontcharouk.webp";
 
 //* Fonts *//
 const vcr = VT323({
@@ -34,7 +34,6 @@ export const metadata = {
     "Explorez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Découvrez ses compétences en JavaScript, HTML, CSS, Sass, React.js et Next.js.",
   ogType: "website",
   ogUrl: "https://alexandre-hontcharouk-portfolio.vercel.app",
-  ogImage: "https://i.postimg.cc/nr93TB6X/Alexandre-Hontcharouk.png",
   ogLocale: "fr_FR",
 };
 
@@ -50,9 +49,33 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content={metadata.ogDescription} />
         <meta property="og:type" content={metadata.ogType} />
         <meta property="og:url" content={metadata.ogUrl} />
-        <meta property="og:image" content="https://i.postimg.cc/nr93TB6X/Alexandre-Hontcharouk.png"/>
-        <meta property="og:image:alt" content="voici l'image de mon portfolio" />
         <meta property="og:locale" content={metadata.ogLocale} />
+        <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+        <meta
+          property="og:image:alt"
+          content="voici l'image de mon portfolio"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://alexandre-hontcharouk-portfolio.vercel.app/"
+        />
+        <meta
+          property="twitter:title"
+          content="Alexandre Hontcharouk - Développeur Web Freelance à Lyon"
+        />
+        <meta
+          property="twitter:description"
+          content="Découvrez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Spécialisé en JavaScript, HTML, CSS, Sass, React.js et Next.js."
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
       </Head>
       <body>
         <AdminContextProvider>
