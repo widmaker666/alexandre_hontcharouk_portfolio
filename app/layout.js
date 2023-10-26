42c116f33a2dea95e4813ba7747523c35eeedbb0
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-10-26 11:39:14 +0200

    maj metadata

--- app/layout.js
+++ app/layout.js
@@ -32,9 +32,21 @@ export const metadata = {
   ogTitle: "Portfolio d'Alexandre Hontcharouk - Développeur Web Freelance",
   ogDescription:
     "Explorez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Découvrez ses compétences en JavaScript, HTML, CSS, Sass, React.js et Next.js.",
-  ogType: "website", 
+  ogType: "website",
   ogUrl: "https://alexandre-hontcharouk-portfolio.vercel.app",
+  ogImage: "./opengraph-image.png",
+  ogImageType: "image/png",
+  ogImageWidth: "1200",
+  ogImageHeight: "630",
+  ogImageAlt: "voici l'image de mon portfolio",
+  ogImageAlt: "voici l'image de mon portfolio",
   ogLocale: "fr_FR",
+  twitterCard: "summary_large_image",
+  twitterUrl: "https://alexandre-hontcharouk-portfolio.vercel.app/",
+  twitterTitle: "Alexandre Hontcharouk - Développeur Web Freelance à Lyon",
+  twitterDescription:
+    "Découvrez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Spécialisé en JavaScript, HTML, CSS, Sass, React.js et Next.js.",
+  twitterImage: "./opengraph-image.png",
 };
 
 export default function RootLayout({ children }) {
@@ -45,40 +57,26 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
+
         <meta property="og:title" content={metadata.ogTitle} />
         <meta property="og:description" content={metadata.ogDescription} />
         <meta property="og:type" content={metadata.ogType} />
         <meta property="og:url" content={metadata.ogUrl} />
         <meta property="og:locale" content={metadata.ogLocale} />
-        <meta
-          property="og:image"
-          content="./opengraph-image.png"
-        />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta
-          property="og:image:alt"
-          content="voici l'image de mon portfolio"
-        />
+        <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:image:type" content={metadata.ogImageType} />
+        <meta property="og:image:width" content={metadata.ogImageWidth} />
+        <meta property="og:image:height" content={metadata.ogImageHeight} />
+        <meta property="og:image:alt" content={metadata.ogImageAlt} />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://alexandre-hontcharouk-portfolio.vercel.app/"
-        />
-        <meta
-          property="twitter:title"
-          content="Alexandre Hontcharouk - Développeur Web Freelance à Lyon"
-        />
+        <meta property="twitter:card" content={metadata.twitterCard} />
+        <meta property="twitter:url" content={metadata.twitterUrl} />
+        <meta property="twitter:title" content={metadata.twitterTitle} />
         <meta
           property="twitter:description"
-          content="Découvrez le portfolio d'Alexandre Hontcharouk, développeur web front-end à Lyon, Villeurbanne. Spécialisé en JavaScript, HTML, CSS, Sass, React.js et Next.js."
+          content={metadata.twitterDescription}
         />
-        <meta
-          property="twitter:image"
-          content="./opengraph-image.png"
-        />
+        <meta property="twitter:image" content={metadata.twitterImage} />
       </Head>
       <body>
         <AdminContextProvider>
